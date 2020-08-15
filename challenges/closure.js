55e06d7b5b65760a06123f40c4161f8c30a4a6b8
Austin Greer <agreer.ag12@gmail.com>
2020-08-14 22:43:18 -0400

    completed closures.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,18 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+/* Explanation: `nestedFunction() is able to access `internal` because a function can access variables 
+outside of its scope. The function is able to see that it does not have `internal`, it looks into the function 
+it's nested in to see if it has the variable. Since that function does, it can log it.
 
 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that 
+number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(randomNum) {
+  return randomNum * (randomNum + 1) / 2;
+}
+
+console.log(summation(6));
